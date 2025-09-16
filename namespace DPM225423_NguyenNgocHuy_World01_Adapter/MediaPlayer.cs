4395796647b65fb65e58ac41d4a804f0dea152c6
Admin <Admin@MT0117>
2025-09-16 10:51:06 +0700

    new

---
+++ namespace DPM225423_NguyenNgocHuy_World01_Adapter/MediaPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace DPM225423_NguyenNgocHuy_World01_Adapter
+{
+    /// <summary>
+    /// The 'Target' interface
+    /// </summary>
+    public interface IMediaPlayer
+    {
+        void Play(string audioType, string fileName);
+    }
+}
